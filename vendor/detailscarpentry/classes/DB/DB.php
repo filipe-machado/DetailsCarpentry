e2bfe4e34bbf371f513c87f28ce9de42cde55740
filipe-machado <ax-filipe@hotmail.com>
2018-07-17 23:14:28 -0500

    Correcting vendor directory

---
+++ vendor/detailscarpentry/classes/DB/DB.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace Details\DB;
+
+class DB {
+
+	const HOSTNAME = "127.0.0.1";
+	const USERNAME = "root";
+	const PASSWORD = "guitar";
+	const DBNAME = "db_ecommerce";
+
+	private $conn;
+
+	public function __construct()
+	{
+
+		$this->conn = new \PDO(
+			"mysql:dbname=".DB::DBNAME.";host=".DB::HOSTNAME, 
+			DB::USERNAME,
+			DB::PASSWORD
+		);
+
+	}
+
+	private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+}
---
+++ vendor/detailscarpentry/classes/PageAdmin.php
@@ -0,0 +1,9 @@
+<?php
+namespace Details;
+class PageAdmin extends Page
+{
+    public function __construct($opts = [], $tpl_dir = "/site/admin/") 
+    {
+        parent::__construct($opts, $tpl_dir);
+    }    
+}
