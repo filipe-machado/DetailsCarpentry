8d3fa2713c6f277eca0aa4b61a3c300a398b3f62
filipe-machado <ax-filipe@hotmail.com>
2018-07-17 23:16:47 -0500

    Correcting vendor directory

--- vendor/detailscarpentry/classes/PageAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Details;
-class PageAdmin extends Page
-{
-    public function __construct($opts = [], $tpl_dir = "/site/admin/") 
-    {
-        parent::__construct($opts, $tpl_dir);
-    }    
-}
